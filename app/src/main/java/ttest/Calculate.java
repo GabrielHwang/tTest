10c2743705fb48242c5d14d3fcbd6892ad646908
Gabriel Hwang <huang.erce@gmail.com>
2024-06-03 00:16:58 +0800

    add treading information func

--- app/src/main/java/ttest/Calculate.java
+++ app/src/main/java/ttest/Calculate.java
@@ -1,29 +1,63 @@
 package ttest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculate {
     int day =0;
     double final_funding = 0;
-    double profit_margin_for_weekday = 0.01;
-    double profit_margin_for_weekend = 0.02;
+    final double fee = 0.00018;
+    final double profit_margin_for_weekday = 0.01;
+    final double profit_margin_for_weekend = 0.02;
 
     /**
-     * calculate how many days will take to target
+     * calculate treading information about total profits and fees
      */
-    public int calculate_days(int initial_funding,int target,int lever) {
-        if (initial_funding > target) {
+    public List<Double> calculateInfo(int initial_funding, int target_funding, int lever) {
+        ArrayList<Double> result = new ArrayList<>();
+        if (initial_funding > target_funding) {
             System.out.println("target should be bigger than initial_founding");
-            return 0;
+            return null;
         }
-        while (initial_funding < target) {
-            double profitOfDay = (day % 7 == 0 || day % 6 == 0) ?
-                    initial_funding * profit_margin_for_weekend:
-                    initial_funding * profit_margin_for_weekday;
-            initial_funding += (int) (profitOfDay * lever);
-            day +=1;
+
+        double profitOfDay = 0;
+        double feeOfDay = 0;
+        double totalFee = 0;
+        double totalProfit = 0;
+        for (int d = 1; initial_funding < target_funding; d++) {
+            profitOfDay = lever * ((d % 7 == 0 || d % 6 == 0) ?
+                    initial_funding * profit_margin_for_weekend :
+                    initial_funding * profit_margin_for_weekday);
+            feeOfDay = initial_funding * lever * fee;
+            initial_funding += (int) (profitOfDay - feeOfDay);
+            day = d;
             final_funding = initial_funding;
-            System.out.println("Day:" + day+"   profit -> " +(int)(profitOfDay * lever)+ "   All: " + final_funding);
+            totalFee += feeOfDay;
+            totalProfit += profitOfDay - feeOfDay;
+            System.out.println("Day:" + day + "   profit -> " + String.format("%.2f", profitOfDay)
+                    + "   fee:   " + String.format("%.2f", feeOfDay)
+                    + "   All: " + final_funding);
         }
-        return day;
+        result.add((double) day);
+        result.add(totalProfit);
+        result.add(totalFee);
+        return result;
+    }
+
+    /**
+     * get total profits from initial to target with lever
+     * */
+    public double getTotalProfit(int initial_funding, int target_funding, int lever) {
+        List<Double> info = calculateInfo(initial_funding, target_funding, lever);
+        return info.get(1);
+    }
+
+    /**
+     * get total fees from initial to target with lever
+     * */
+    public double getTotalFee(int initial_funding, int target_funding, int lever) {
+        List<Double> info = calculateInfo(initial_funding, target_funding, lever);
+        return info.get(2);
     }
 
 }
--- app/src/test/java/ttest/CalculateTest.java
+++ app/src/test/java/ttest/CalculateTest.java
@@ -2,13 +2,26 @@ package ttest;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculateTest {
     @Test
     public void calculateTest() {
         Calculate calculate = new Calculate();
-        int result = calculate.calculate_days(400, 4000, 10);
-        assertEquals(19,result);
+        List<Double> calculateInfoTest = calculate.calculateInfo(400, 4000, 10);
+        System.out.println(calculateInfoTest);
+        assertEquals(21.0, calculateInfoTest.get(0));
+        assertEquals(4394.5276, calculateInfoTest.get(1));
+        assertEquals(59.972400000000015,calculateInfoTest.get(2));
+
+    }
+    @Test
+    public void getTotalFeeTest(){
+        Calculate calculate =new Calculate();
+        double totalFee = calculate.getTotalFee(400, 4000, 10);
+        assertEquals(59.972400000000015, totalFee);
     }
 }
